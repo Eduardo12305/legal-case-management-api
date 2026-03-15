23692abbd19e6fdead77c8f3abba212ea7f1ac3a
Eduardo12305 <eumarinho@outlook.com>
2026-03-15 17:22:07 -0300

    Use app base URL in server startup log

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app.jsx');
 const env = require('./config/env.jsx');
 
 app.listen(env.port, () => {
-  console.log(`Server is running on http://localhost:${env.port}`);
+  console.log(`Server is running on ${env.appBaseUrl}`);
 });
