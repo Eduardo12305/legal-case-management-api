46d59d0221e7f835ee0d88140085911e063250d9
Eduardo12305 <eumarinho@outlook.com>
2026-03-15 22:00:43 -0300

    Improve SSE flushing for chat realtime

--- src/services/chatRealtimeService.jsx
+++ src/services/chatRealtimeService.jsx
@@ -19,7 +19,13 @@ class ChatRealtimeService {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache, no-transform');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no');
+    res.setHeader('Content-Encoding', 'identity');
     res.flushHeaders?.();
+
+    // Helps some proxies/clients start streaming immediately.
+    res.write(': connected\n\n');
+    res.flush?.();
 
     this.sendEvent(res, 'connected', {
       userId,
@@ -79,6 +85,7 @@ class ChatRealtimeService {
   sendEvent(res, eventName, payload) {
     res.write(`event: ${eventName}\n`);
     res.write(`data: ${JSON.stringify(payload)}\n\n`);
+    res.flush?.();
   }
 }
 
